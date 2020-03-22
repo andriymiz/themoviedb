3bbfd16be4322f3f2522dc8a572688928febba54
Andriy Miz <andriy.miz.v@gmail.com>
2020-03-22 19:30:39 +0200

    remove my methods

--- src/AccountV3.js
+++ src/AccountV3.js
@@ -23,12 +23,11 @@ export default {
    * @param {string} options.api_key - Required
    * @param {string} options.language
    * @param {string} options.session_id - Required
-   * @param {('created_at.asc'|'created_at.desc')} options.sort_by
    * @param {number} options.page
    * @returns {Promise}
    * @memberof TheMovieDb
    */
-  getLists(account_id, options) {
+  getLists(account_id = (this.v3_account || {id:0}).id, options) {
     return this.getV3(`account/${account_id}/lists`, {
       ...this.metas(["api_key", "language", "session_id"]),
       ...options
@@ -36,22 +35,6 @@ export default {
   },
 
   /**
-   * Get all of the lists created by an account. Will
-   * invlude private lists if you are the owner.
-   * @param {Object} options
-   * @param {string} options.api_key - Required
-   * @param {string} options.language
-   * @param {string} options.session_id - Required
-   * @param {('created_at.asc'|'created_at.desc')} options.sort_by
-   * @param {number} options.page
-   * @returns {Promise}
-   * @memberof TheMovieDb
-   */
-  getMyLists(options) {
-    return this.getLists(this.v3_account ? this.v3_account.id : 0, options);
-  },
-
-  /**
    * Get the list of your favorite movies.
    * @param {number} account_id
    * @param {Object} options
@@ -63,7 +46,7 @@ export default {
    * @returns {Promise}
    * @memberof TheMovieDb
    */
-  getFavoritesMovies(account_id, options) {
+  getFavoritesMovies(account_id = (this.v3_account || {id:0}).id, options) {
     return this.getV3(`account/${account_id}/favorite/movies`, {
       ...this.metas(["api_key", "language", "session_id"]),
       ...options
@@ -71,24 +54,6 @@ export default {
   },
 
   /**
-   * Get the list of your favorite movies.
-   * @param {Object} options
-   * @param {string} options.api_key - Required
-   * @param {string} options.language
-   * @param {string} options.session_id - Required
-   * @param {('created_at.asc'|'created_at.desc')} options.sort_by
-   * @param {number} options.page
-   * @returns {Promise}
-   * @memberof TheMovieDb
-   */
-  getMyFavoritesMovies(options) {
-    return this.getFavoritesMovies(
-      this.v3_account ? this.v3_account.id : 0,
-      options
-    );
-  },
-
-  /**
    * Get the list of your favorite TV shows.
    * @param {number} account_id
    * @param {Object} options
@@ -100,29 +65,11 @@ export default {
    * @returns {Promise}
    * @memberof TheMovieDb
    */
-  getFavoritesTvShows(account_id, options) {
+  getFavoritesTvShows(account_id = (this.v3_account || {id:0}).id, options) {
     return this.getV3(`account/${account_id}/favorite/tv`, {
       ...this.metas(["api_key", "language", "session_id"]),
       ...options
     });
-  },
-
-  /**
-   * Get the list of your favorite TV shows.
-   * @param {Object} options
-   * @param {string} options.api_key - Required
-   * @param {string} options.language
-   * @param {string} options.session_id - Required
-   * @param {('created_at.asc'|'created_at.desc')} options.sort_by
-   * @param {number} options.page
-   * @returns {Promise}
-   * @memberof TheMovieDb
-   */
-  getMyFavoritesTvShows(options) {
-    return this.getFavoritesTvShows(
-      this.v3_account ? this.v3_account.id : 0,
-      options
-    );
   },
 
   /**
@@ -139,7 +86,7 @@ export default {
    * @returns {Promise}
    * @memberof TheMovieDb
    */
-  markAsFavorite(account_id, options, body) {
+  markAsFavorite(account_id = (this.v3_account || {id:0}).id, options, body) {
     return this.postV3(
       `account/${account_id}/favorite`,
       {
@@ -151,22 +98,6 @@ export default {
   },
 
   /**
-   * This method allows you to mark a movie or TV show as
-   * a favorite item.
-   * @param {Object} options
-   * @param {string} options.api_key - Required
-   * @param {string} options.session_id - Required
-   * @returns {Promise}
-   * @memberof TheMovieDb
-   */
-  markAsMyFavorite(options) {
-    return this.markAsFavorite(
-      this.v3_account ? this.v3_account.id : 0,
-      options
-    );
-  },
-
-  /**
    * Get a list of all the movies you have rated.
    * @param {number} account_id
    * @param {Object} options
@@ -178,7 +109,7 @@ export default {
    * @returns {Promise}
    * @memberof TheMovieDb
    */
-  getRatedMovies(account_id, options) {
+  getRatedMovies(account_id = (this.v3_account || {id:0}).id, options) {
     return this.getV3(`account/${account_id}/rated/movies`, {
       ...this.metas(["api_key", "language", "session_id"]),
       ...options
@@ -186,24 +117,6 @@ export default {
   },
 
   /**
-   * Get a list of all the movies you have rated.
-   * @param {Object} options
-   * @param {string} options.api_key - Required
-   * @param {string} options.language
-   * @param {string} options.session_id - Required
-   * @param {('created_at.asc'|'created_at.desc')} options.sort_by
-   * @param {number} options.page
-   * @returns {Promise}
-   * @memberof TheMovieDb
-   */
-  getMyRatedMovies(options) {
-    return this.getRatedMovies(
-      this.v3_account ? this.v3_account.id : 0,
-      options
-    );
-  },
-
-  /**
    * Get a list of all the TV shows you have rated.
    * @param {number} account_id
    * @param {Object} options
@@ -215,7 +128,7 @@ export default {
    * @returns {Promise}
    * @memberof TheMovieDb
    */
-  getRatedTvShows(account_id, options) {
+  getRatedTvShows(account_id = (this.v3_account || {id:0}).id, options) {
     return this.getV3(`account/${account_id}/rated/tv`, {
       ...this.metas(["api_key", "language", "session_id"]),
       ...options
@@ -223,24 +136,6 @@ export default {
   },
 
   /**
-   * Get a list of all the TV shows you have rated.
-   * @param {Object} options
-   * @param {string} options.api_key - Required
-   * @param {string} options.language
-   * @param {string} options.session_id - Required
-   * @param {('created_at.asc'|'created_at.desc')} options.sort_by
-   * @param {number} options.page
-   * @returns {Promise}
-   * @memberof TheMovieDb
-   */
-  getMyRatedTvShows(options) {
-    return this.getRatedTvShows(
-      this.v3_account ? this.v3_account.id : 0,
-      options
-    );
-  },
-
-  /**
    * Get a list of all the TV episodes you have rated.
    * @param {number} account_id
    * @param {Object} options
@@ -252,7 +147,7 @@ export default {
    * @returns {Promise}
    * @memberof TheMovieDb
    */
-  getRatedTvEpisodes(account_id, options) {
+  getRatedTvEpisodes(account_id = (this.v3_account || {id:0}).id, options) {
     return this.getV3(`account/${account_id}/rated/tv/episodes`, {
       ...this.metas(["api_key", "language", "session_id"]),
       ...options
@@ -260,24 +155,6 @@ export default {
   },
 
   /**
-   * Get a list of all the TV episodes you have rated.
-   * @param {Object} options
-   * @param {string} options.api_key - Required
-   * @param {string} options.language
-   * @param {string} options.session_id - Required
-   * @param {('created_at.asc'|'created_at.desc')} options.sort_by
-   * @param {number} options.page
-   * @returns {Promise}
-   * @memberof TheMovieDb
-   */
-  getMyRatedTvEpisodes(options) {
-    return this.getRatedTvEpisodes(
-      this.v3_account ? this.v3_account.id : 0,
-      options
-    );
-  },
-
-  /**
    * Get a list of all the movies you have added to your
    * watchlist.
    * @param {number} account_id
@@ -290,30 +167,11 @@ export default {
    * @returns {Promise}
    * @memberof TheMovieDb
    */
-  getMovieWatchlist(account_id, options) {
+  getMovieWatchlist(account_id = (this.v3_account || {id:0}).id, options) {
     return this.getV3(`account/${account_id}/watchlist/movies`, {
       ...this.metas(["api_key", "language", "session_id"]),
       ...options
     });
-  },
-
-  /**
-   * Get a list of all the movies you have added to your
-   * watchlist.
-   * @param {Object} options
-   * @param {string} options.api_key - Required
-   * @param {string} options.language
-   * @param {string} options.session_id - Required
-   * @param {('created_at.asc'|'created_at.desc')} options.sort_by
-   * @param {number} options.page
-   * @returns {Promise}
-   * @memberof TheMovieDb
-   */
-  getMyMovieWatchlist(options) {
-    return this.getMovieWatchlist(
-      this.v3_account ? this.v3_account.id : 0,
-      options
-    );
   },
 
   /**
@@ -329,30 +187,11 @@ export default {
    * @returns {Promise}
    * @memberof TheMovieDb
    */
-  getTvShowWatchlist(account_id, options) {
+  getTvShowWatchlist(account_id = (this.v3_account || {id:0}).id, options) {
     return this.getV3(`account/${account_id}/watchlist/movies`, {
       ...this.metas(["api_key", "language", "session_id"]),
       ...options
     });
-  },
-
-  /**
-   * Get a list of all the TV shows you have added to your
-   * watchlist.
-   * @param {Object} options
-   * @param {string} options.api_key - Required
-   * @param {string} options.language
-   * @param {string} options.session_id - Required
-   * @param {('created_at.asc'|'created_at.desc')} options.sort_by
-   * @param {number} options.page
-   * @returns {Promise}
-   * @memberof TheMovieDb
-   */
-  getMyTvShowWatchlist(options) {
-    return this.getTvShowWatchlist(
-      this.v3_account ? this.v3_account.id : 0,
-      options
-    );
   },
 
   /**
@@ -360,36 +199,22 @@ export default {
    * @param {number} account_id
    * @param {Object} options
    * @param {string} options.api_key - Required
-   * @param {string} options.language
    * @param {string} options.session_id - Required
-   * @param {('created_at.asc'|'created_at.desc')} options.sort_by
-   * @param {number} options.page
+   * @param {Object} body
+   * @param {('movie'|'tv')} body.media_type - Required
+   * @param {number} body.media_id - Required
+   * @param {boolean} body.watchlist - Required
    * @returns {Promise}
    * @memberof TheMovieDb
    */
-  addToWatchlist(account_id, options) {
+  addToWatchlist(account_id = (this.v3_account || {id:0}).id, options, body) {
     return this.postV3(
       `account/${account_id}/watchlist`,
-      this.metas(["api_key", "language", "session_id"]),
-      options
-    );
-  },
-
-  /**
-   * Add a movie or TV show to your watchlist.
-   * @param {Object} options
-   * @param {string} options.api_key - Required
-   * @param {string} options.language
-   * @param {string} options.session_id - Required
-   * @param {('created_at.asc'|'created_at.desc')} options.sort_by
-   * @param {number} options.page
-   * @returns {Promise}
-   * @memberof TheMovieDb
-   */
-  addToMyWatchlist(options) {
-    return this.addToWatchlist(
-      this.v3_account ? this.v3_account.id : 0,
-      options
+      {
+        ...this.metas(["api_key", "session_id"]),
+        ...options
+      },
+      body
     );
   }
 };
--- src/AccountV4.js
+++ src/AccountV4.js
@@ -8,22 +8,11 @@ export default {
    * @returns {Promise}
    * @memberof TheMovieDb
    */
-  getV4Lists(account_id, options) {
+  getV4Lists(account_id = (this.v4_account || {id:0}).id, options) {
     return this.getV4(`account/${account_id}/lists`, options);
   },
 
   /**
-   * Get all of the lists you've created.
-   * @param {Object} options
-   * @param {number} options.page
-   * @returns {Promise}
-   * @memberof TheMovieDb
-   */
-  getMyV4Lists(options) {
-    return this.getV4Lists(this.v4_account ? this.v4_account.id : 0, options);
-  },
-
-  /**
    * Get the list of movies you have marked as a favorite.
    * @param {number} account_id
    * @param {Object} options
@@ -32,26 +21,11 @@ export default {
    * @returns {Promise}
    * @memberof TheMovieDb
    */
-  getV4FavoritesMovies(account_id, options) {
+  getV4FavoritesMovies(account_id = (this.v4_account || {id:0}).id, options) {
     return this.getV4(`account/${account_id}/movie/favorites`, options);
   },
 
   /**
-   * Get the list of movies you have marked as a favorite.
-   * @param {Object} options
-   * @param {('created_at.asc'|'created_at.desc'|'release_date.asc'|'release_date.desc'|'title.asc'|'title.desc'|'vote_average.asc'|'vote_average.desc')} options.sort_by
-   * @param {number} options.page
-   * @returns {Promise}
-   * @memberof TheMovieDb
-   */
-  getMyV4FavoritesMovies(options) {
-    return this.getV4FavoritesMovies(
-      this.v4_account ? this.v4_account.id : 0,
-      options
-    );
-  },
-
-  /**
    * Get the list of TV shows you have marked as a favorite.
    * @param {number} account_id
    * @param {Object} options
@@ -60,26 +34,11 @@ export default {
    * @returns {Promise}
    * @memberof TheMovieDb
    */
-  getV4FavoritesTvShows(account_id, options) {
+  getV4FavoritesTvShows(account_id = (this.v4_account || {id:0}).id, options) {
     return this.getV4(`account/${account_id}/tv/favorites`, options);
   },
 
   /**
-   * Get the list of TV shows you have marked as a favorite.
-   * @param {Object} options
-   * @param {('created_at.asc'|'created_at.desc'|'release_date.asc'|'release_date.desc'|'title.asc'|'title.desc'|'vote_average.asc'|'vote_average.desc')} options.sort_by
-   * @param {number} options.page
-   * @returns {Promise}
-   * @memberof TheMovieDb
-   */
-  getMyV4FavoritesTvShows(options) {
-    return this.getV4FavoritesTvShows(
-      this.v4_account ? this.v4_account.id : 0,
-      options
-    );
-  },
-
-  /**
    * Get a list of your personal movie recommendations.
    * @param {number} account_id
    * @param {Object} options
@@ -88,26 +47,11 @@ export default {
    * @returns {Promise}
    * @memberof TheMovieDb
    */
-  getV4MovieRecommendations(account_id, options) {
+  getV4MovieRecommendations(account_id = (this.v4_account || {id:0}).id, options) {
     return this.getV4(`account/${account_id}/movie/recommendations`, options);
   },
 
   /**
-   * Get a list of your personal movie recommendations.
-   * @param {Object} options
-   * @param {('created_at.asc'|'created_at.desc'|'release_date.asc'|'release_date.desc'|'title.asc'|'title.desc'|'vote_average.asc'|'vote_average.desc')} options.sort_by
-   * @param {number} options.page
-   * @returns {Promise}
-   * @memberof TheMovieDb
-   */
-  getMyV4MovieRecommendations(options) {
-    return this.getV4MovieRecommendations(
-      this.v4_account ? this.v4_account.id : 0,
-      options
-    );
-  },
-
-  /**
    * Get a list of your personal TV show recommendations.
    * @param {number} account_id
    * @param {Object} options
@@ -116,26 +60,11 @@ export default {
    * @returns {Promise}
    * @memberof TheMovieDb
    */
-  getV4TvShowRecommendations(account_id, options) {
+  getV4TvShowRecommendations(account_id = (this.v4_account || {id:0}).id, options) {
     return this.getV4(`account/${account_id}/tv/recommendations`, options);
   },
 
   /**
-   * Get a list of your personal TV show recommendations.
-   * @param {Object} options
-   * @param {('created_at.asc'|'created_at.desc'|'release_date.asc'|'release_date.desc'|'title.asc'|'title.desc'|'vote_average.asc'|'vote_average.desc')} options.sort_by
-   * @param {number} options.page
-   * @returns {Promise}
-   * @memberof TheMovieDb
-   */
-  getMyV4TvShowRecommendations(options) {
-    return this.getV4TvShowRecommendations(
-      this.v4_account ? this.v4_account.id : 0,
-      options
-    );
-  },
-
-  /**
    * Get the list of movies you have added to your watchlist.
    * @param {number} account_id
    * @param {Object} options
@@ -144,26 +73,11 @@ export default {
    * @returns {Promise}
    * @memberof TheMovieDb
    */
-  getV4MovieWatchlist(account_id, options) {
+  getV4MovieWatchlist(account_id = (this.v4_account || {id:0}).id, options) {
     return this.getV4(`account/${account_id}/movie/watchlist`, options);
   },
 
   /**
-   * Get the list of movies you have added to your watchlist.
-   * @param {Object} options
-   * @param {('created_at.asc'|'created_at.desc'|'release_date.asc'|'release_date.desc'|'title.asc'|'title.desc'|'vote_average.asc'|'vote_average.desc')} options.sort_by
-   * @param {number} options.page
-   * @returns {Promise}
-   * @memberof TheMovieDb
-   */
-  getMyV4MovieWatchlist(options) {
-    return this.getV4MovieWatchlist(
-      this.v4_account ? this.v4_account.id : 0,
-      options
-    );
-  },
-
-  /**
    * Get the list of TV shows you have added to your watchlist.
    * @param {number} account_id
    * @param {Object} options
@@ -172,26 +86,11 @@ export default {
    * @returns {Promise}
    * @memberof TheMovieDb
    */
-  getV4TvShowWatchlist(account_id, options) {
+  getV4TvShowWatchlist(account_id = (this.v4_account || {id:0}).id, options) {
     return this.getV4(`account/${account_id}/tv/watchlist`, options);
   },
 
   /**
-   * Get the list of TV shows you have added to your watchlist.
-   * @param {Object} options
-   * @param {('created_at.asc'|'created_at.desc'|'release_date.asc'|'release_date.desc'|'title.asc'|'title.desc'|'vote_average.asc'|'vote_average.desc')} options.sort_by
-   * @param {number} options.page
-   * @returns {Promise}
-   * @memberof TheMovieDb
-   */
-  getMyV4TvShowWatchlist(options) {
-    return this.getV4TvShowWatchlist(
-      this.v4_account ? this.v4_account.id : 0,
-      options
-    );
-  },
-
-  /**
    * Get the list of movies you have rated.
    * @param {number} account_id
    * @param {Object} options
@@ -200,26 +99,11 @@ export default {
    * @returns {Promise}
    * @memberof TheMovieDb
    */
-  getV4RatedMovies(account_id, options) {
+  getV4RatedMovies(account_id = (this.v4_account || {id:0}).id, options) {
     return this.getV4(`account/${account_id}/movie/rated`, options);
   },
 
   /**
-   * Get the list of movies you have rated.
-   * @param {Object} options
-   * @param {('created_at.asc'|'created_at.desc'|'release_date.asc'|'release_date.desc'|'title.asc'|'title.desc'|'vote_average.asc'|'vote_average.desc')} options.sort_by
-   * @param {number} options.page
-   * @returns {Promise}
-   * @memberof TheMovieDb
-   */
-  getMyV4RatedMovies(options) {
-    return this.getV4RatedMovies(
-      this.v4_account ? this.v4_account.id : 0,
-      options
-    );
-  },
-
-  /**
    * Get the list of TV shows you have rated.
    * @param {number} account_id
    * @param {Object} options
@@ -228,22 +112,7 @@ export default {
    * @returns {Promise}
    * @memberof TheMovieDb
    */
-  getV4RatedTvShows(account_id, options) {
+  getV4RatedTvShows(account_id = (this.v4_account || {id:0}).id, options) {
     return this.getV4(`account/${account_id}/tv/rated`, options);
-  },
-
-  /**
-   * Get the list of TV shows you have rated.
-   * @param {Object} options
-   * @param {('created_at.asc'|'created_at.desc'|'release_date.asc'|'release_date.desc'|'title.asc'|'title.desc'|'vote_average.asc'|'vote_average.desc')} options.sort_by
-   * @param {number} options.page
-   * @returns {Promise}
-   * @memberof TheMovieDb
-   */
-  getMyV4RatedTvShows(options) {
-    return this.getV4RatedTvShows(
-      this.v4_account ? this.v4_account.id : 0,
-      options
-    );
   }
 };
